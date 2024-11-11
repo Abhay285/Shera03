51126e8a63032d497372389d29f3e00700ba5ab1
Abhay kaiwartya <abhaykaiwartya2019@gmail.com>
2024-11-11 20:12:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,10 +2,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
+API_ID       = int(environ.get("API_ID", "29034951"))
+API_HASH     = environ.get("API_HASH", "cfc8448de6746a6ccf9dc1812cf1c8f7")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "")
 DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002464849807"))
+ADMIN        = int(environ.get("ADMIN", "1328531459"))
 CHANNEL      = environ.get("CHANNEL", "@RMCBACKUP")
