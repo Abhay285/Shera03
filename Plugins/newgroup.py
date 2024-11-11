2a4ee819f578c520d79fb031b4ae414a94e27223
Abhay kaiwartya <abhaykaiwartya2019@gmail.com>
2024-11-11 19:38:47 +0530

    Update newgroup.py

--- Plugins/newgroup.py
+++ Plugins/newgroup.py
@@ -1,26 +1,44 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 from info import *
 from utils import *
 from asyncio import sleep
-from pyrogram import Client, filters 
+from pyrogram import Client, filters
+import traceback
 
 @Client.on_message(filters.group & filters.new_chat_members)
 async def new_group(bot, message):
-    bot_id = (await bot.get_me()).id
-    member = [u.id for u in message.new_chat_members]        
-    if bot_id in member:
-       await add_group(group_id=message.chat.id, 
-                       group_name=message.chat.title,
-                       user_name=message.from_user.first_name, 
-                       user_id=message.from_user.id, 
-                       channels=[],
-                       f_sub=False,
-                       verified=False)
-       m=await message.reply(f"💢 <b>Thanks for adding me in {message.chat.title} ✨\n\n⭕ Please Get Access By /verify</b>\n\n")
-       text=f"#NewGroup\n\nGroup: {message.chat.title}\nGroupID: `{message.chat.id}`\nAddedBy: {message.from_user.mention}\nUserID: `{message.from_user.id}`"
-       await bot.send_message(chat_id=LOG_CHANNEL, text=text)
-       await sleep(60)
-       await m.delete()
+    try:
+        bot_id = (await bot.get_me()).id
+        member_ids = [u.id for u in message.new_chat_members]
+
+        # Check if bot is added to the group
+        if bot_id in member_ids:
+            group_data = {
+                "group_id": message.chat.id,
+                "group_name": message.chat.title,
+                "user_name": message.from_user.first_name,
+                "user_id": message.from_user.id,
+                "channels": [],
+                "f_sub": False,
+                "verified": False
+            }
+
+            # Add group data to the database
+            await add_group(**group_data)
+
+            # Send confirmation message to the group
+            confirmation_message = await message.reply(
+                f"💢 <b>Thanks for adding me to {message.chat.title} ✨\n\n⭕ Please Get Access By /verify</b>\n\n"
+            )
+
+            # Log the group details
+            log_text = f"#NewGroup\n\nGroup: {message.chat.title}\nGroupID: `{message.chat.id}`\nAddedBy: {message.from_user.mention}\nUserID: `{message.from_user.id}`"
+            await bot.send_message(chat_id=LOG_CHANNEL, text=log_text)
+
+            # Wait for 60 seconds before deleting the confirmation message
+            await sleep(60)
+            await confirmation_message.delete()
+
+    except Exception as e:
+        # In case of an error, log the exception for debugging
+        print(f"Error in new_group handler: {e}")
+        traceback.print_exc()
